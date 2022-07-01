3e02a7117e3acdf928c73d153ce942de12212d72
Shas <shaswarbaban@gmail.com>
2022-07-01 04:36:27 +0100

    adding new author/contributor name and new python version

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ kwargs = {
     'test_suite': 'test_strconv',
     'name': 'strconv',
     'version': strconv.__version__,
-    'author': 'Byron Ruth',
+    'author': 'Byron Ruth, Shaswar Baban',
     'author_email': 'b@devel.io',
     'description': 'String type inference and conversion',
     'license': 'BSD',
     'keywords': 'types inference conversion strings',
-    'url': 'https://github.com/bruth/strconv/',
+    'url': 'https://github.com/shasthemass/strconv/',
     'classifiers': [
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: BSD License',
@@ -19,6 +19,7 @@ kwargs = {
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.9.10',
     ],
 }
 
